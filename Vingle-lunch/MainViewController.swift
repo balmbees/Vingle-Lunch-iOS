50109a76ebc55e889bdd5b372ba8caf93cd81e6f
Tylor Shin <tylor.shin@vingle.net>
2016-03-31 14:00:09 +0900

    init commit

---
+++ Vingle-lunch/MainViewController.swift
@@ -0,0 +1,24 @@
+//
+//  MainViewController.swift
+//  Vingle-lunch
+//
+//  Created by SHINMILL on 3/30/16.
+//  Copyright © 2016 SHINMILL. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
